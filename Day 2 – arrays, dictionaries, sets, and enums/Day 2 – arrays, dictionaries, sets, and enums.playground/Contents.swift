bebc06c3f790c18947d60cb507ca78ef503e54ab
Tolga Pirtürk <tpirturk@gmail.com>
2021-07-31 19:25:57 +0300

    Arrays
    Add beatles array with string typed constants.

--- Day 2 – arrays, dictionaries, sets, and enums/Day 2 – arrays, dictionaries, sets, and enums.playground/Contents.swift
+++ Day 2 – arrays, dictionaries, sets, and enums/Day 2 – arrays, dictionaries, sets, and enums.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+beatles[1]
