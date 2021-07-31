034dde4e3102cc965fa4fc44da3c617add8e6370
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-01 02:51:34 +0300

    Enum associated value
    Add Activity enum with associated values.

--- Day 2 – arrays, dictionaries, sets, and enums/Day 2 – arrays, dictionaries, sets, and enums.playground/Contents.swift
+++ Day 2 – arrays, dictionaries, sets, and enums/Day 2 – arrays, dictionaries, sets, and enums.playground/Contents.swift
@@ -56,3 +56,11 @@ enum Conclusion {
 }
 let conclusion4 = Conclusion.failure
 
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+
+let talking = Activity.talking(topic: "football")
