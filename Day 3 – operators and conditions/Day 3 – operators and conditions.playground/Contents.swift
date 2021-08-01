95da3887f1b144915de3d6453954db7c4193367f
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-02 01:21:30 +0300

    Switch statements
    Add weather switch statements.

--- Day 3 – operators and conditions/Day 3 – operators and conditions.playground/Contents.swift
+++ Day 3 – operators and conditions/Day 3 – operators and conditions.playground/Contents.swift
@@ -67,3 +67,28 @@ if firstCard2 == secondCard2 {
 } else {
     print("Cards are different")
 }
+
+let weather = "sunny"
+
+switch weather {
+    case "rain":
+        print("Bring an umbrella")
+    case "snow":
+        print("Wrap up warm")
+    case "sunny":
+        print("Wear sunscreen")
+    default:
+        print("Enjoy your day!")
+}
+
+switch weather {
+    case "rain":
+        print("Bring an umbrella")
+    case "snow":
+        print("Wrap up warm")
+    case "sunny":
+        print("Wear sunscreen")
+        fallthrough
+    default:
+        print("Enjoy your day!")
+}
