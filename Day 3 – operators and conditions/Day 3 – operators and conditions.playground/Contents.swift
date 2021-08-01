b4c48af814002cf64f76cfcd668e425ab9792616
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-02 01:07:20 +0300

    Conditions
    Add if, else and else if statements.

--- Day 3 – operators and conditions/Day 3 – operators and conditions.playground/Contents.swift
+++ Day 3 – operators and conditions/Day 3 – operators and conditions.playground/Contents.swift
@@ -35,3 +35,14 @@ firstScore2 < secondScore2
 firstScore2 >= secondScore2
 
 "Taylor" <= "Swift"
+
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 2 {
+    print("Aces - lucky!")
+} else if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
