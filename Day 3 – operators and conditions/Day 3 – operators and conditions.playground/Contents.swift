ed649fcde284735a31286c50b9b560f13bcc436d
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-02 01:25:55 +0300

    Range operators
    Use range operators with a switch statement.

--- Day 3 – operators and conditions/Day 3 – operators and conditions.playground/Contents.swift
+++ Day 3 – operators and conditions/Day 3 – operators and conditions.playground/Contents.swift
@@ -92,3 +92,14 @@ switch weather {
     default:
         print("Enjoy your day!")
 }
+
+let score2 = 85
+
+switch score2 {
+    case 0..<50:
+        print("You failed badly.")
+    case 50..<85:
+        print("You did OK.")
+    default:
+        print("You did great!.")
+}
