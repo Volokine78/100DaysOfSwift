e626aa54676e6254ead0826dc42583f64e5b172b
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-03 00:34:54 +0300

    While loops
    Add a while loop for hide and seek game.

--- Day 4 – loops, loops, and more loops/Day 4 – loops, loops, and more loops .playground/Contents.swift
+++ Day 4 – loops, loops, and more loops/Day 4 – loops, loops, and more loops .playground/Contents.swift
@@ -17,3 +17,12 @@ print("Players gonna ")
 for _ in 1...5 {
     print("play")
 }
+
+var number = 1
+
+while number <= 20 {
+    print(number)
+    number += 1
+}
+
+print("Ready or not, here I come!")
