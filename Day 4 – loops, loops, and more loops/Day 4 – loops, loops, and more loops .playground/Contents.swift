81f0a2d0bd35ef455891a35c04de1d4bc444c06f
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-03 00:46:48 +0300

    Exiting loop
    Use break keyword to exit while loop.

--- Day 4 – loops, loops, and more loops/Day 4 – loops, loops, and more loops .playground/Contents.swift
+++ Day 4 – loops, loops, and more loops/Day 4 – loops, loops, and more loops .playground/Contents.swift
@@ -39,3 +39,18 @@ print("Ready or not, here I come!")
 repeat {
     print("This is false")
 } while false
+
+var countDown = 10
+
+while countDown >= 0 {
+    print(countDown)
+    
+    if countDown == 4 {
+        print("I'm bored. Let's go now!")
+        break
+    }
+    
+    countDown -= 1
+}
+
+print("Blast off!")
