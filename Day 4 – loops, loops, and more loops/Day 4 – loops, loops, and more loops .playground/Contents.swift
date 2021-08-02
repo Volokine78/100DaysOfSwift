069a2ee3728414dd0fcf263c7cab7b220e5d3af1
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-03 01:41:11 +0300

    Exiting multiple loops
    Use break keyword to exit multiple loops.

--- Day 4 – loops, loops, and more loops/Day 4 – loops, loops, and more loops .playground/Contents.swift
+++ Day 4 – loops, loops, and more loops/Day 4 – loops, loops, and more loops .playground/Contents.swift
@@ -54,3 +54,15 @@ while countDown >= 0 {
 }
 
 print("Blast off!")
+
+outerloop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print("\(i) * \(j) is \(product)")
+        
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerloop
+        }
+    }
+}
