4812a005b29a5c3449b7b7c9dadc17aeb89b5295
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-03 19:11:23 +0300

    Writing functions
    Add printHelp function and call the function.

--- Day 5 – functions, parameters, and errors/Day 5 – functions, parameters, and errors.playground/Contents.swift
+++ Day 5 – functions, parameters, and errors/Day 5 – functions, parameters, and errors.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func printHelp() {
+    let message = """
+welcome to MyApp!
+
+Run this app inside a directory of images and
+MyApp will resize them all into thumbnails
+"""
+    print(message)
+}
+
+printHelp()
