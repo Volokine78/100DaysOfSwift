6ee0aef20c3f4f3e1261d3da2dd69c8cad1d17ca
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-06 01:07:31 +0300

    Using closures as parameters when they accept parameters
    Use a closure, which accepts parameter as a parameter to a function.

--- Day 7 – closures part two/Day 7 – closures part two.playground/Contents.swift
+++ Day 7 – closures part two/Day 7 – closures part two.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func travel(action: (String) -> Void) {
+    print("I'm getting ready to go.")
+    action("London")
+    print("I arrived")
+}
+
+travel { (place: String) in
+    print("I'm going to \(place) in my car")
+}
