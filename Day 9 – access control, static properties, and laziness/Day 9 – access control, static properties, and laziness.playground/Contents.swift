70a34ecacef3776a07e41f14f8766ca5f8284ee9
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-09 02:56:26 +0300

    Initializers
    Add examples of struct initializers.

--- Day 9 – access control, static properties, and laziness/Day 9 – access control, static properties, and laziness.playground/Contents.swift
+++ Day 9 – access control, static properties, and laziness/Day 9 – access control, static properties, and laziness.playground/Contents.swift
@@ -1,3 +1,51 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct User {
+    var username: String
+    
+    init() {
+        username = "Anonymous"
+        print("Creating a new user!")
+    }
+}
+
+var user = User()
+print(user.username)
+user.username = "twostraws"
+print(user.username)
+
+struct User2 {
+    var name: String
+    var yearsActive = 0
+}
+
+let roslin = User2(name: "Laura Roslin")
+let adama = User2(name: "William Adama", yearsActive: 45)
+
+struct Employee {
+    var name: String
+    var yearsActive = 0
+    
+    init() {
+        name = "Anonymous"
+        print("Creating anonymous employee.")
+    }
+}
+
+let laura = Employee()
+
+struct Employee2 {
+    var name: String
+    var yearsActive = 0
+}
+
+extension Employee2 {
+    init() {
+        name = "Anonymous"
+        print("Creating an anonymous employee..")
+    }
+}
+
+let adam = Employee2(name: "Adam Silver", yearsActive: 46)
+let anon = Employee2()
+
