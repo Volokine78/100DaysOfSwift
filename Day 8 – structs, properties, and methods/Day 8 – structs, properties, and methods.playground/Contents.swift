dbd3435944583338d2d3a1a2bc2d8cb80cc16434
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-08 23:13:52 +0300

    Creating your own structs
    Add Sport struct as an example.

--- Day 8 – structs, properties, and methods/Day 8 – structs, properties, and methods.playground/Contents.swift
+++ Day 8 – structs, properties, and methods/Day 8 – structs, properties, and methods.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Sport {
+    var name: String
+}
+
+var tennis = Sport(name: "Tennis")
+print(tennis.name)
+
+tennis.name = "Lawn tennis"
+print(tennis.name)
