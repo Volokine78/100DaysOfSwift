8ecb5d77a55540df25cd0a74c6f5c161a3dfb7ad
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-09 02:04:21 +0300

    Properties and methods of strings
    Add some examples of properties and methods of string.

--- Day 8 – structs, properties, and methods/Day 8 – structs, properties, and methods.playground/Contents.swift
+++ Day 8 – structs, properties, and methods/Day 8 – structs, properties, and methods.playground/Contents.swift
@@ -62,3 +62,10 @@ struct Person {
 var person = Person(name: "Ed")
 person.makeAnonymous()
 print(person.name)
+
+let string = "Do or do not, there is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
