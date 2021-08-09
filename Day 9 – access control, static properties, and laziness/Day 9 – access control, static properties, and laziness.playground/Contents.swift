cdeb280ebeabe54091ceabb40101db77417482f6
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-09 03:08:27 +0300

    Referring to the current instance
    Add struct examples, which uses special constant called self in their methods.

--- Day 9 – access control, static properties, and laziness/Day 9 – access control, static properties, and laziness.playground/Contents.swift
+++ Day 9 – access control, static properties, and laziness/Day 9 – access control, static properties, and laziness.playground/Contents.swift
@@ -49,3 +49,22 @@ extension Employee2 {
 let adam = Employee2(name: "Adam Silver", yearsActive: 46)
 let anon = Employee2()
 
+struct Person {
+    var name: String
+    
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
+
+struct Student {
+    var name: String
+    var bestFriend: String
+    
+    init(name: String, bestFriend: String) {
+        print("Enrolling \(name) in class...")
+        self.name = name
+        self.bestFriend = bestFriend
+    }
+}
