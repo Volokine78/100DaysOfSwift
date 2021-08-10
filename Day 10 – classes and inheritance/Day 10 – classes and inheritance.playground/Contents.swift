bd1fd306fc1413da6a9b2ea1c2fea7b4abb7715b
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-10 21:37:30 +0300

    Copying objects
    Add an example of copying objects.

--- Day 10 – classes and inheritance/Day 10 – classes and inheritance.playground/Contents.swift
+++ Day 10 – classes and inheritance/Day 10 – classes and inheritance.playground/Contents.swift
@@ -42,3 +42,15 @@ final class Dog3 {
         self.breed = breed
     }
 }
+
+class Singer {
+    var name = "Taylor Swift"
+}
+
+var singer = Singer()
+print(singer.name)
+
+var singerCopy = singer
+singerCopy.name = "Justin Bieber"
+
+print(singer.name)
