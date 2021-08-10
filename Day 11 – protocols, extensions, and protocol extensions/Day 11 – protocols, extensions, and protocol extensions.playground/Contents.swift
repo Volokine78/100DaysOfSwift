b18d96bcfdd31bebeb8f6e80aa804b073a87d102
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-11 00:08:12 +0300

    Protocols
    Add examples of protocols and structs that conforms them.

--- Day 11 – protocols, extensions, and protocol extensions/Day 11 – protocols, extensions, and protocol extensions.playground/Contents.swift
+++ Day 11 – protocols, extensions, and protocol extensions/Day 11 – protocols, extensions, and protocol extensions.playground/Contents.swift
@@ -1,3 +1,46 @@
 import UIKit
 
-var greeting = "Hello, playground"
+protocol Identifiable {
+    var id: String { get set}
+}
+
+struct User: Identifiable {
+    var id: String
+}
+
+func displayID(thing: Identifiable) {
+    print("My ID is \(thing.id)")
+}
+
+let john = User(id: "123456")
+displayID(thing: john)
+
+struct Book: Purchaseable {
+    var name: String
+    var author: String
+}
+
+func buy(_ book: Book) {
+    print("I'm buying \(book.name)")
+}
+
+protocol Purchaseable {
+    var name: String { get set }
+}
+
+struct Movie: Purchaseable {
+    var name: String
+    var actors: [String]
+}
+
+struct Car: Purchaseable {
+    var name: String
+    var manufacturer: String
+}
+
+func buy(_ item: Purchaseable) {
+    print("I'm buying \(item.name)")
+}
+
+let porche = Car(name: "911 turbo", manufacturer: "Porche")
+buy(porche)
