3d445ae22f2a7376d5b41db96054737363d55ae1
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-10 22:47:53 +0300

    Deinitializers
    Add an object with a deinitializer.

--- Day 10 – classes and inheritance/Day 10 – classes and inheritance.playground/Contents.swift
+++ Day 10 – classes and inheritance/Day 10 – classes and inheritance.playground/Contents.swift
@@ -54,3 +54,24 @@ var singerCopy = singer
 singerCopy.name = "Justin Bieber"
 
 print(singer.name)
+
+class Person {
+    var name = "John Doe"
+    
+    init() {
+        print("\(name) is alive!")
+    }
+    
+    func printGreeting() {
+        print("Hello, I'm \(name)")
+    }
+    
+    deinit {
+        print("\(name) is no more!")
+    }
+}
+
+for _ in 1...3 {
+    let person = Person()
+    person.printGreeting()
+}
