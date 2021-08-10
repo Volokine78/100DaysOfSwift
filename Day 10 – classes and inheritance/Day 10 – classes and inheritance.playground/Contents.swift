3e69b8dfb9ee1c1e8a24a4f5200e7f52d62b76c4
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-10 20:42:53 +0300

    Creating your own classes
    Add a class example.

--- Day 10 – classes and inheritance/Day 10 – classes and inheritance.playground/Contents.swift
+++ Day 10 – classes and inheritance/Day 10 – classes and inheritance.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class Dog {
+    var name: String
+    var breed: String
+    
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+let poopy = Dog(name: "Poppy", breed: "Poodle")
