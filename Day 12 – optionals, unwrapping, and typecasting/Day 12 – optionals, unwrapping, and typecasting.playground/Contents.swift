da93d3311a9a21a353157892ae03a28acc19e3aa
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-11 02:41:24 +0300

    Unwrapping optionals
    Add unwrapped optionals.

--- Day 12 – optionals, unwrapping, and typecasting/Day 12 – optionals, unwrapping, and typecasting.playground/Contents.swift
+++ Day 12 – optionals, unwrapping, and typecasting/Day 12 – optionals, unwrapping, and typecasting.playground/Contents.swift
@@ -2,3 +2,21 @@ import UIKit
 
 var age: Int? = nil
 age = 38
+
+var name: String? = "John"
+
+if let unwrapped = name {
+    print("\(unwrapped.count) letters")
+} else {
+    print("Missing name.")
+}
+
+func getUsername() -> String? {
+    "Taylor"
+}
+
+if let username = getUsername() {
+    print("username is \(username)")
+} else {
+    print("No Username")
+}
