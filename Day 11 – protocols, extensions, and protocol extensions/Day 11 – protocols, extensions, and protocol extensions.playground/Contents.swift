39d6f6ab86d37d82adbb153736201c185a115930
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-11 00:28:13 +0300

    Protocol inheritance
    Add examples of protocol inheritance.

--- Day 11 – protocols, extensions, and protocol extensions/Day 11 – protocols, extensions, and protocol extensions.playground/Contents.swift
+++ Day 11 – protocols, extensions, and protocol extensions/Day 11 – protocols, extensions, and protocol extensions.playground/Contents.swift
@@ -44,3 +44,32 @@ func buy(_ item: Purchaseable) {
 
 let porche = Car(name: "911 turbo", manufacturer: "Porche")
 buy(porche)
+
+protocol Payable {
+    func calculateWages() -> Int
+}
+
+protocol NeedsTraining {
+    func study()
+}
+
+protocol HasVacation {
+    func takeVacation(days: Int)
+}
+
+protocol Employee: Payable, NeedsTraining, HasVacation { }
+
+protocol Product {
+    var price: Double { get set }
+    var weight: Int { get set }
+}
+
+protocol Computer: Product {
+    var cpu: String { get set }
+    var memory: Int { get set }
+    var storage: Int { get set }
+}
+
+protocol Laptop: Computer {
+    var screenSize: Int { get set }
+}
