8800d4df67131bdcff825b1e67985e87f14fbcdc
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-11 01:01:57 +0300

    Protocol extensions
    Add an extension to a protocol.

--- Day 11 – protocols, extensions, and protocol extensions/Day 11 – protocols, extensions, and protocol extensions.playground/Contents.swift
+++ Day 11 – protocols, extensions, and protocol extensions/Day 11 – protocols, extensions, and protocol extensions.playground/Contents.swift
@@ -89,3 +89,31 @@ extension Int {
     }
 }
 number.isEven
+
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+        
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()
+
+let numbers = [4, 8, 15, 16]
+let allEven = numbers.allSatisfy { $0.isMultiple(of: 2) }
+
+let numbers2 = Set([4, 8, 15, 16])
+let allEven2 = numbers2.allSatisfy { $0.isMultiple(of: 2) }
+
+let numbers3 = ["four": 4, "eight": 8, "fifteen": 15, "sixteen": 16]
+let allEven3 = numbers3.allSatisfy { $0.value.isMultiple(of: 2) }
+print(allEven, allEven2, allEven3)
+
+
