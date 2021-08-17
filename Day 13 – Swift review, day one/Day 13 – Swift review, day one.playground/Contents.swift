69ce4bcc41474a4a38bcc28b4d56a46e0ca8d832
Tolga Pirtürk <tpirturk@gmail.com>
2021-08-17 03:31:56 +0300

    Types of Data
    Add examples of data types.

--- Day 13 – Swift review, day one/Day 13 – Swift review, day one.playground/Contents.swift
+++ Day 13 – Swift review, day one/Day 13 – Swift review, day one.playground/Contents.swift
@@ -2,3 +2,27 @@ import UIKit
 
 var name = "Tim McGraw"
 name = "Romeo"
+
+var age: Int
+age = 25
+
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Float
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
+
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat: Bool
+missABeat = false
+
