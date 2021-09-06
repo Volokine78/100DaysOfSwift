f8f5b8cc6991c0c81c3013ee13919e2f216628b6
Tolga Pirtürk <tpirturk@gmail.com>
2021-09-06 17:26:25 +0300

    Loops
    Add different loop examples.

--- Day 13 – Swift review, day one/Day 13 – Swift review, day one.playground/Contents.swift
+++ Day 13 – Swift review, day one/Day 13 – Swift review, day one.playground/Contents.swift
@@ -115,3 +115,59 @@ if stayOutTooLate3 && nothingInBrai3 {
 if !stayOutTooLate3 && !nothingInBrai3 {
     action = "cruise"
 }
+
+for i in 1...10 {
+    print("\(i) x 10 is \(i * 10)")
+}
+
+var str = "Fakers gonna"
+
+for _ in 1...5 {
+    str += " fake"
+}
+print(str)
+
+var newSongs = ["Shake it off", "You Belong with Me", "Look What You Made Me Do"]
+for song in newSongs {
+    print("My favorite song is \(song)")
+}
+
+var people = ["players", "haters", "heart-breakers", "fakers"]
+var actions = ["play", "hate", "break", "fake"]
+
+for i in 0...3 {
+    print("\(people[i]) gonna \(actions[i])")
+}
+
+for i in 0..<people.count {
+    print("\(people[i]) gonna \(actions[i])")
+}
+
+for i in 0..<people.count {
+    var str = "\(people[i]) gonna"
+    
+    for _ in 1...5 {
+        str += " \(actions[i])"
+    }
+    
+    print(str)
+}
+
+var counter = 0
+
+while true {
+    print("Counter is now \(counter)")
+    counter += 1
+    
+    if counter == 20 {
+        break
+    }
+}
+
+for song in newSongs {
+    if song == "You Belong with Me" {
+        continue
+    }
+    
+    print("My favorite song is \(song)")
+}
