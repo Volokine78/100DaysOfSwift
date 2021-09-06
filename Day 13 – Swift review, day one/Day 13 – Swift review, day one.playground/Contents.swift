3ac651d06acfa3b9df88ee5abd7c79f932e3c3c2
Tolga Pirtürk <tpirturk@gmail.com>
2021-09-06 16:10:42 +0300

    Operators
    Add examples of operators.

--- Day 13 – Swift review, day one/Day 13 – Swift review, day one.playground/Contents.swift
+++ Day 13 – Swift review, day one/Day 13 – Swift review, day one.playground/Contents.swift
@@ -26,3 +26,33 @@ nothingInBrain = true
 var missABeat: Bool
 missABeat = false
 
+var a = 10
+a = a + 1
+a = a - 1
+a = a * a
+
+var b = 10
+b += 10
+b -= 10
+
+var c = 1.1
+var d = 2.2
+var e = c + d
+
+var name1 = "Tim McGraw"
+var name2 = "Romeo"
+var both = name1 + " and " + name2
+
+e > 3
+e >= 3
+e > 4
+e < 4
+
+var name3 = "Tim McGraw"
+name3 == "Tim McGraw"
+
+var stayOutTooLate2 = true
+stayOutTooLate2
+!stayOutTooLate2
+
+name3 != "Tim McGraw"
