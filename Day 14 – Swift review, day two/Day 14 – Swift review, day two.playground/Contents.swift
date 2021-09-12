7d8c6be6943a858fce05b381765c2da3841e8266
Tolga Pirtürk <tpirturk@gmail.com>
2021-09-12 17:42:14 +0300

    Optionals
    Add examples for optionals.

--- Day 14 – Swift review, day two/Day 14 – Swift review, day two.playground/Contents.swift
+++ Day 14 – Swift review, day two/Day 14 – Swift review, day two.playground/Contents.swift
@@ -39,3 +39,59 @@ if albumIsTaylor(name: "Blue") {
 } else {
     print("Who made that?")
 }
+
+func getHaterStatus(weather: String) -> String? {
+    if weather == "sunny" {
+        return nil
+    } else {
+        return "Hate"
+    }
+}
+
+var status: String?
+status = getHaterStatus(weather: "rainy")!
+
+func takeHaterAction(status: String) {
+    if status == "Hate" {
+        print("Hating")
+    }
+}
+
+if let haterStatus = getHaterStatus(weather: "rainy") {
+    takeHaterAction(status: haterStatus)
+}
+
+func yearAlbummReleased(name: String) -> Int? {
+    if name == "Taylor Swift" { return 2006 }
+    if name == "Fearless" { return 2008 }
+    if name == "Speak Now" { return 2010 }
+    if name == "Red" { return 2012}
+    if name == "1989" { return 2014}
+    
+    return nil
+}
+
+var year = yearAlbummReleased(name: "Red")
+
+if year == nil {
+    print("There was an error")
+} else {
+    print("It was released in \(year!)")
+}
+
+var items = ["James", "John", "Sally"]
+
+func position(of string: String, in array: [String]) -> Int? {
+    for i in 0..<array.count {
+        if array[i] == string {
+            return i
+        }
+    }
+    return nil
+}
+
+let jamesPosition = position(of: "James", in: items)
+let johnPosition = position(of: "John", in: items)
+let sallyPosition = position(of: "Sally", in: items)
+let bobPosition = position(of: "Bob", in: items)
+
