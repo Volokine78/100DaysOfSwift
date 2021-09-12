d442a8b769b6c9a609e6d22e3bf3c634802f25fd
Tolga Pirtürk <tpirturk@gmail.com>
2021-09-12 16:52:26 +0300

    Functions
    Add examples of functions for review.

--- Day 14 – Swift review, day two/Day 14 – Swift review, day two.playground/Contents.swift
+++ Day 14 – Swift review, day two/Day 14 – Swift review, day two.playground/Contents.swift
@@ -1,2 +1,41 @@
 import UIKit
 
+func favoriteAlbum(name: String) {
+    print("My favorite is \(name)")
+}
+
+favoriteAlbum(name: "Fearless")
+
+func printAlbumRelease(name: String, year: Int) {
+    print("\(name) was released in \(year)")
+}
+
+printAlbumRelease(name: "Fearless", year: 2008)
+printAlbumRelease(name: "Speak Now", year: 2010)
+printAlbumRelease(name: "Red", year: 2012)
+
+func countLettersInString(in string: String) {
+    print("The string \(string) has \(string.count) letters.")
+}
+
+countLettersInString(in: "Hello")
+
+func albumIsTaylor(name: String) -> Bool {
+    if name == "Taylor Swift" { return true }
+    if name == "Fearless" { return true }
+    if name == "Speak Now" { return true}
+    
+    return false
+}
+
+if albumIsTaylor(name: "Fearless") {
+    print("That's one of hers!")
+} else {
+    print("Who made that?")
+}
+
+if albumIsTaylor(name: "Blue") {
+    print("That's one of hers!")
+} else {
+    print("Who made that?")
+}
